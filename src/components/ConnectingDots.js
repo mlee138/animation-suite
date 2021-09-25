03bc5cf65b4c210bc874fbe7e5ee388e98b1bea8
Matt <mlee138@binghamton.edu>
2021-09-25 12:59:44 -0400

    dots draw/update on screen

--- src/components/ConnectingDots.js
+++ src/components/ConnectingDots.js
@@ -8,6 +8,75 @@ function ConnectingDots() {
     const canvasRef = useRef(null);
 
     useEffect(()=>{
+        const canvas = canvasRef.current;
+        canvas.width = containerRef.current.offsetWidth;
+        canvas.height = containerRef.current.offsetHeight;
+        const ctx = canvas.getContext('2d');
+
+        const mouse = {
+            x: undefined,
+            y: undefined
+        }
+
+        const updateMouse =(e)=>{
+            mouse.x = e.x;
+            mouse.y = e.y;
+        }
+        window.addEventListener('mousemove', updateMouse);
+
+        class Dot {
+            constructor(x, y, dy, r){
+                this.x = x;
+                this.y = y;
+                this.dy = dy;
+                this.r = r;
+            }
+
+            draw(){
+                ctx.beginPath();
+                ctx.arc(this.x, this.y, this.r, 0, Math.PI*2);
+                ctx.fillStyle = '#faeece';
+                ctx.fill();
+                ctx.closePath();
+            }
+
+            update(){
+                if (this.y-this.r > canvas.height) this.y = -this.r;
+                this.y += this.dy;
+
+                this.draw();
+            }
+        }
+
+        const init=()=>{
+            const arr = [];
+            const r = 5;
+            for(let i=0; i<30;i++){
+                arr.push(new Dot(
+                                Math.random()*(canvas.width+r*2) - r,
+                                Math.random() * canvas.height,
+                                Math.random()*1,
+                                r 
+                            )          
+                )
+            }
+            return arr;
+        }
+        const dots = init();
+        let animationFrameId;
+        const render =()=>{
+            ctx.clearRect(0,0, canvas.width, canvas.height);
+            dots.forEach(dot=>{
+                dot.update();
+            })
+            animationFrameId = window.requestAnimationFrame(render);
+        }
+        render();
+
+        return () => {
+            window.removeEventListener('mousemove', updateMouse);
+            window.cancelAnimationFrame(animationFrameId);
+        }
 
     }, []);
 
