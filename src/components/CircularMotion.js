c5f98f97db17f1c5eed5ab404d7ec5c6efa94681
Matt <mlee138@binghamton.edu>
2021-09-27 19:32:16 -0400

    coded circular motion functionality

--- src/components/CircularMotion.js
+++ src/components/CircularMotion.js
@@ -6,11 +6,84 @@ function CircularMotion(){
     const canvasRef = useRef(null);
 
     useEffect(()=>{
+        const canvas = canvasRef.current;
+        canvas.width = containerRef.current.offsetWidth;
+        canvas.height = containerRef.current.offsetHeight;
+        const halfWidth = canvas.width/2;
+        const halfHeight = canvas.height/2;
+        const ctx = canvas.getContext('2d');
+        let animationFrameId;
+        let speedUp = false;
 
+        const toggleSpeed =()=> speedUp = !speedUp;
+
+        window.addEventListener('mousedown', toggleSpeed);
+        window.addEventListener('mouseup', toggleSpeed);
+
+
+        class Circle{
+            constructor(r, color, sa) {
+                this.r = r;
+                this.color = color;
+                this.startAngle = sa;
+                this.length = 8;
+                this.speed = 0.002;
+            }
+            
+            draw() {
+                //might add a gradient to the stroke
+                ctx.beginPath();
+                const endAngle = this.startAngle+Math.PI * (this.speed*5);
+                ctx.arc(halfWidth, halfHeight, this.r, this.startAngle, endAngle);
+                ctx.lineCap = 'round';
+                ctx.lineWidth = 2;
+                ctx.strokeStyle = this.color;
+                ctx.stroke();
+                ctx.closePath();
+            }
+
+            update(){
+                const change = 1.01;
+                if(speedUp && this.speed < 0.1) 
+                    this.speed *= change;
+                else if(!speedUp && this.speed > 0.002) 
+                    this.speed /= change;
+                
+                this.startAngle += this.speed;
+                this.draw();
+            }
+        }
+
+        let circles = [];
+        const colors = ['#2d6e7f', '#6da3b5', '#24646b', '#18303e', '#0e4c74']
+        
+        const init=()=>{
+            for(let i=0; i<60; i++){
+                circles.push( new Circle(Math.random()*500 + 20, 
+                                        colors[Math.floor(Math.random()*colors.length)],
+                                        Math.random()*Math.PI*2 )
+                );
+            }
+        }
+        
+
+        const render = () => {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            circles.forEach(circle=> circle.update() )
+            animationFrameId = window.requestAnimationFrame(render)
+        }
+        init()
+        render()
+        
+        return () => {
+            window.removeEventListener('mousedown', toggleSpeed);
+            window.removeEventListener('mouseup', toggleSpeed);
+            window.cancelAnimationFrame(animationFrameId);
+        }
     }, []);
 
     return(
-        <CanvasContainer containerRef={containerRef} title='Around The World' date='9-27-2021'>
+        <CanvasContainer containerRef={containerRef} title='Warp Speed' date='9-27-2021' bg='black'>
             <canvas ref={canvasRef}/>
         </CanvasContainer>
     );
