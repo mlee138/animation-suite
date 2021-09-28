1e78f94cb8cc97435d65dc847e007cb10ab4110c
Matt <mlee138@binghamton.edu>
2021-09-27 21:29:19 -0400

    finished sliding screen

--- src/components/SlidingScreen.js
+++ src/components/SlidingScreen.js
@@ -4,40 +4,41 @@ import styled from 'styled-components';
 
 function SlidingScreen(){
     const [width, setWidth] = useState(0);
+    
+    const containerRef = useRef(null);
 
     useEffect(()=>{
-
+        const updateMouse=(e)=> setWidth(e.x);
+        window.addEventListener('mousemove', updateMouse);
     }, []);
 
     return(
-        <Container title='Sliding Screen' date='9-27-2021'>
-                <Side bg='orange' color='hotPink'>left</Side>
-                <Side bg='hotPink' color='orange'>right</Side>
+        <Container containerRef={containerRef} title='Sliding Screen' date='9-27-2021'>
+                <Side left='0' right={window.innerWidth - width} bg='orange' color='hotPink'>left</Side>
+                <Side left={width} right='0' bg='hotPink' color='orange'>right</Side>
         </Container>
     );
 }
 
-/*const Side = styled.div.attrs(props => ({
+const Side = styled.div.attrs(props => ({
     style: {
-        width: `${props.width}`
+        right: props.right,
+        left: props.left
     }
-}))*/
-
-const Side = styled.div`
+}))`
     display: flex;
     justify-content: center;
     align-items: center;
     position: absolute;
     top: 0;
-    left: 0;
     bottom: 0;
-    right: 0;
     background-color: ${props => props.bg};
     color: ${props => props.color};
     font-weight: bold;
     font-family: 'Bebas Neue', cursive;
     font-size: 15vw;
     letter-spacing: 0.25em;
+    overflow: hidden;
 `;
 
 export default SlidingScreen;
